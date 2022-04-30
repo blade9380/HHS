333c6e2e9ad121b559f4f81c4a163a7a60259f83
blade9380 <trnblade@gmail.com>
2022-04-30 23:28:53 +0630

    thrid commit

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import time
+
+import requests.packages
 from flask import *
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
@@ -5,6 +8,7 @@ from flask_wtf import FlaskForm
 from wtforms import *
 import requests
 import random
+from datetime import datetime
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -42,27 +46,49 @@ def maths():
     global correct
     global question_id
     if request.method == 'GET':
-        question_id += 1
-        print(question_id)
-        data = Data.query.get(question_id)
-        answers = data.answer.split(',')
-        return render_template('math.html', data=data, answers=answers, id=question_id, correct=correct*10)
-
+        if question_id <= 9:
+            question_id += 1
+            data = Data.query.get(question_id)
+            answers = data.answer.split(',')
+            return render_template('math.html', data=data, answers=answers, id=question_id, correct=correct)
+        else:
+            correct = 0
+            question_id = 1
+            data = Data.query.get(question_id)
+            answers = data.answer.split(',')
+            time.sleep(10)
+            return render_template('math.html', data=data, answers=answers, id=question_id, correct=correct)
     if request.method == 'POST':
         data = Data.query.get(question_id)
-        print(request.form['options'])
-        print(data.correct_answer)
         if request.form['options'] == data.correct_answer:
             correct += 1
-            print(correct)
-            return redirect(url_for('maths', id=question_id, correct=correct*10))
-        return redirect(url_for('maths', id=question_id, correct=correct*10))
+            return redirect(url_for('maths', id=question_id, correct=correct))
+        return redirect(url_for('maths', id=question_id, correct=correct))
 
 
+@app.route('/bla', methods=['GET', 'POST'])
+def bla():
+    global question_id
+    global correct
+    point = correct
+    question_no = question_id
+    correct = 0
+    question_id = 0
+    if request.method == 'POST':
+        print('hello')
+        print(request.form['options'])
+        if request.form['options'] == '1':
+            point += 1
+            return render_template('bla.html', point=point, question_no=question_no)
+    return render_template('bla.html', point=point, question_no=question_no)
 
-@app.route('/bla/<int:id>')
-def bla(id):
-    return render_template('bla.html', id=1)
+
+@app.route('/books')
+def books():
+    data = requests.get(url="https://api.itbook.store/1.0/new").json()
+    free = [i for i in data['books'] if float(i['price'][1::]) < 1]
+    print(free)
+    return render_template('books.html', data=free)
 
 
 if __name__ == '__main__':
